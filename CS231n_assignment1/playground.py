81c28d65f21b12d9d5af7488fb4257b4c35d1229
Probe <probe301@gmail.com>
2016-10-13 16:46:02 +0800

    start softmax

--- CS231n_assignment1/playground.py
+++ CS231n_assignment1/playground.py
@@ -1,7 +1,15 @@
 
-import random
-import numpy as np
-from cs231n.data_utils import load_CIFAR10
-import matplotlib.pyplot as plt
-a = 100
-data = np.array([float('nan')] * a)
+# import random
+# import numpy as np
+# from cs231n.data_utils import load_CIFAR10
+# import matplotlib.pyplot as plt
+# a = 100
+# data = np.array([float('nan')] * a)
+
+import json
+from pprint import pprint
+
+with open('doodles_demo.json') as json_data:
+    d = json.loads(json_data.read())
+    json_data.close()
+    pprint(d)
