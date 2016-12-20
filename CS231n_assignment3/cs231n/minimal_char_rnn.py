28616e0ff84727fed3d631f15d46008dea58d478
Probe <probe301@gmail.com>
2016-12-20 15:02:23 +0800

    read minimal char rnn

--- CS231n_assignment3/cs231n/minimal_char_rnn.py
+++ CS231n_assignment3/cs231n/minimal_char_rnn.py
@@ -7,7 +7,7 @@ BSD License
 import numpy as np
 
 # data I/O
-data = open('input.txt', 'r').read() # should be simple plain text file
+data = open('input.txt', 'r', encoding='utf-8').read() # should be simple plain text file
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print('data has %d characters, %d unique.' % (data_size, vocab_size))
@@ -33,16 +33,24 @@ def lossFun(inputs, targets, hprev):
   returns the loss, gradients on model parameters, and last hidden state
   """
   xs, hs, ys, ps = {}, {}, {}, {}
+
+  # xs shape V, 1
   hs[-1] = np.copy(hprev)
   loss = 0
   # forward pass
   for t in range(len(inputs)):
     xs[t] = np.zeros((vocab_size, 1)) # encode in 1-of-k representation
     xs[t][inputs[t]] = 1
+
+    # np.dot(Wxh, xs[t]) shape H, 1
+    # hs shape H, 1
     hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
+    # ys shape V, 1
     ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
     ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
     loss += -np.log(ps[t][targets[t], 0]) # softmax (cross-entropy loss)
+
+
   # backward pass: compute gradients going backwards
   dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
   dbh, dby = np.zeros_like(bh), np.zeros_like(by)
@@ -61,6 +69,8 @@ def lossFun(inputs, targets, hprev):
   for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
     np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
   return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
+
+
 
 def sample(h, seed_ix, n):
   """
